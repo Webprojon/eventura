401739f35b42f6ff5181eb001d90a3929be1248b
Webprojon <tokhirjonkhasanov@gmail.com>
2025-05-08 21:33:51 +0200

    .

--- src/components/input-components/AuthLayout.tsx
+++ src/components/input-components/AuthLayout.tsx
@@ -12,8 +12,8 @@ export default function AuthLayout({ subtitle, children }: AuthLayoutProps) {
 			variants={smoothOpacity}
 			className="max-w-[1350px] mx-auto min-h-[100vh] flex justify-center items-center"
 		>
-			<div className="flex items-center gap-x-10">
-				<div className="flex flex-col items-center justify-center gap-y-4 flex-1">
+			<div className="flex items-center justify-center flex-wrap gap-10">
+				<div className="flex flex-col items-center justify-center sm:gap-y-4 flex-1">
 					<Link to="/" className="flex items-center gap-3">
 						<FaPeopleGroup className="size-14 text-sky-300" />
 						<h1 className="leading-none font-semibold text-[32px] md:text-[50px] text-center">Eventura</h1>
@@ -21,7 +21,7 @@ export default function AuthLayout({ subtitle, children }: AuthLayoutProps) {
 					<p className="text-xl tracking-wider text-slate-300">{subtitle}</p>
 				</div>
 
-				<div className="h-[70vh] border-r"></div>
+				<div className="hidden md:block h-[70vh] border-r"></div>
 
 				<div>{children}</div>
 			</div>
--- src/pages/home-page/Starter.tsx
+++ src/pages/home-page/Starter.tsx
@@ -7,17 +7,12 @@ import { fromTopToBottom } from "../../lib/page-animations";
 export default function StarterPage() {
 	return (
 		<motion.section className="flex items-center justify-center h-[100vh]">
-			<motion.div
-				initial="initial"
-				animate="animate"
-				variants={fromTopToBottom}
-				className="flex flex-col gap-y-4 items-center justify-center z-50"
-			>
+			<motion.div initial="initial" animate="animate" variants={fromTopToBottom} className="flex flex-col gap-y-4 items-center justify-center z-50">
 				<div className="flex items-center gap-3">
 					<FaPeopleGroup className="size-12 text-sky-300" />
 					<h1 className="leading-none font-semibold text-[46px]">Eventura</h1>
 				</div>
-				<p className="text-lg text-slate-300">Your gateway to discovering and joining amazing events around you.</p>
+				<p className="text-lg text-slate-300 text-center px-2">Your gateway to discovering and joining amazing events around you.</p>
 				<Link to="/events" className="flex items-center gap-x-2 group mt-6 py-[10px] px-[46px] btn">
 					START
 					<MdArrowForward className="size-5 transform transition-transform duration-200 group-hover:translate-x-1" />
